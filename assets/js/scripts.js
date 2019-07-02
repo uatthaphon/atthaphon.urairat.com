48ef8dee47179a408a230bc19b6509227cad4db3
Atthaphon Urairat <aurairat@inetasia.com>
2019-07-02 09:54:12 +0700

    update tech blog from dev.to and add production compress css

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 (function() {
   ran_col();
-
+  feedParse();
 })();
 
 function ran_col() { //function name
@@ -15,6 +15,33 @@ function ran_col() { //function name
   document.querySelectorAll('.hero .content .social .item .link').forEach(function(el) {
     el.style.color = color;
   })
+}
 
+function feedParse() {
+//feed to parse
+  var feed = "https://dev.to/feed/uatthaphon";
 
+  $.ajax(feed, {
+    accepts:{
+      xml:"application/rss+xml"
+    },
+    dataType:"xml",
+    success:function(data) {
+      $(data).find("item").each(function (i, item) { // or "item" or whatever suits your feed
+        var el = $(this);
+        var techBlogUl = $('ul#tech-blog');
+        var link = el.find("link").text();
+        var title = el.find("title").text();
+        var summary = $('<div/>').html(el.find("description").text()).text();
+        var summaryCount = 100;
+        summary = summary.slice(0, summaryCount) + (summary.length > summaryCount ? "..." : "");
+
+        techBlogUl.append('<li class="item"><a href="'+link+'" class="link">'+title+'</a><p>'+summary+'</p></li>');
+        // console.log("description: " + $('<div/>').html(el.find("description").text().trim()).text().trim());
+        // console.log("------------------------");
+
+        return i < 4; // only 5 row
+      });
+    }
+  });
 }
