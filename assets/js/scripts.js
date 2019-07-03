378e7d40f25c5cdf7db9a1a1742f3f7fd1fbaef1
Atthaphon Urairat <aurairat@inetasia.com>
2019-07-03 16:59:42 +0700

    update color

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,8 +6,8 @@
 function ran_col() { //function name
   var color = '#'; // hexadecimal starting symbol
   var letters = [
-    //'#fae2af', '#f6c1bc', '#c3dbf5', '#c9f9e5', '#e3e6ea'
-    'fae2af', 'f6c1bc', 'c3dbf5', 'c9f9e5', 'e3e6ea'
+    //'#53c1ba', '#ffe600', '#ffffff', '#eeeeee',
+    '53c1ba', 'ffe600', 'ffffff', 'eeeeee',
   ]; //Set your colors here
   color += letters[Math.floor(Math.random() * letters.length)];
   document.body.style.background = color; // Setting the random color on your div element.
